c88019151e88c2e29f02eb27c9265f15bb7b3972
Gil Vitzinger <gil.vitzinger@excelero.com>
2019-05-01 13:58:40 +0300

    Deployment - node-driver DaemonSet
    Change-Id: I6e2fe021cde20fbd22d38c863825e094169fe5a3
    Signed-off-by: Gil <gil.vitzinger@excelero.com>

--- build/developer_tools/clear_old_deployment.sh
+++ build/developer_tools/clear_old_deployment.sh
@@ -4,5 +4,7 @@ kubectl delete service nvmesh-csi-controller
 kubectl delete serviceaccount nvmesh-csi
 kubectl delete statefulsets nvmesh-csi-controller
 kubectl delete pod nvmesh-csi-controller-0
+kubectl delete daemonset nvmesh-csi-node-driver
+kubectl delete pod nvmesh-csi-node-driver
 
 #kubectl delete pods,services,serviceaccounts,statefulsets -l app=nvmesh-csi
