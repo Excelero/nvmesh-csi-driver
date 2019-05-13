79a650b4a92c4ffc7b571c4ce0597308b07cefac
Gil Vitzinger <gil.vitzinger@excelero.com>
2019-05-13 14:00:10 +0300

    Support Build on remote machines
    *build.sh updated to recevie --servers argument with array of remote machines to build on
    *README.md updated
    
    Change-Id: Ie1281db4a0167237a0184e900bef13917b0caeb6
    Signed-off-by: Gil <gil.vitzinger@excelero.com>

--- README.md
+++ README.md
@@ -23,16 +23,23 @@ This section covers the deployment per CO system
 #### Kubernetes
 Deployment of NVMesh CSI driver consists of the following stages:
 
-##### 1. Upload docker image to docker registry
+##### 1. Build & Upload Docker image
 
-Upload the driver container image (nvmesh-csi-plugin) to a private docker registry deployed at your site, or to each nodes local docker registry
+Upload the driver container image (nvmesh-csi-plugin) to a private Docker registry deployed at your site, or to each nodes local Docker registry
 
-You can use the `build_tools/build.sh` script to build the image with on hte current docker environment
+From a machine with SSH access to all nodes run the following:
 
-##### 2. load resources & deploy driver
+    cd ./build_tools
+    ./build.sh --servers node1 node2 node3
 
-Run the following command on a node with kubectl cli
+* where node1,node2,node3 are the Kubernetes Cluster server names including the master node
 
+##### 2. Load resources & deploy driver
+
+2.1 Run the following command on **one** node with kubectl cli
+
+    # the sources will be in ~/nvmesh_csi_driver/ if you followed step 1
+    cd ~/nvmesh_csi_driver/
     cd ./deploy
     ./deploy.sh
 
@@ -46,11 +53,13 @@ resources include:
  * nvmesh storage classes
 
 ##### 3. Configuration
-3.1 make sure the selected namespace is `nvmesh-csi`
 
-3.2 edit `management.servers` to your MANAGEMENT_SERVERS configuration in `Config Maps` > `nvmesh-config`
+Using Dashboard:
+3.1 make sure the selected namespace is `nvmesh-csi` in the side menu
 
-3.3 edit `username` and `password` to your management server credentials configuration in `Secrets` > `nvmesh-credentials`
+3.2 Go to `Config Maps` > `nvmesh-config` and edit `management.servers` to your MANAGEMENT_SERVERS configuration
 
-
-
+3.3 Go to `Secrets` > `nvmesh-credentials` and edit `username` and `password` to your management server credentials configuration
+ * secret in Kubernetes must be in base64 format
+ * for example use: `echo -n 'admin@excelero.com' | base64` and `echo -n 'admin' | base64` to get the username and passsword in base64
+ * for more info visit: [Kubernetes Docs - Convert your secret data to a base-64 representation](https://kubernetes.io/docs/tasks/inject-data-application/distribute-credentials-secure/#convert-your-secret-data-to-a-base-64-representation)
--- build_tools/build.sh
+++ build_tools/build.sh
@@ -1,13 +1,79 @@
 #!/usr/bin/env bash
 
-cd ../
-# using the -f flag allows us to include files from a directory out of the 'context'
-# we need it because the Dockerfile is in build dir and sources are in driver dir
+servers=()
 
-# NOTE: building while the snx VPN is running could cause network issues that will fail building the image
-docker build -f build_tools/nvmesh-csi-plugin.dockerfile . --tag excelero/nvmesh-csi-plugin:v0.0.1
+show_help() {
+    echo "Usage: ./build.sh [--servers s1 s2 s3]"
+    echo "To build to local docker registry use: ./build.sh"
+    echo "To build on remote machines using ssh use: ./build.sh --servers kube-master kube-node-1 kube-node-2"
+}
 
-if [ $? -ne 0 ]; then
-    echo "Docker image build failed"
-    exit 1
+parse_args() {
+    #temp_args=( $@ )
+    while [[ $# -gt 0 ]]
+    do
+    key="$1"
+
+    case $key in
+        -s|--servers)
+
+        shift # skip "--servers" arg
+
+        while [[ "$1" != -* && $# -gt 0 ]]; do
+            servers+=("$1")
+            shift
+        done
+
+        ;;
+        -h|--help)
+            show_help
+            exit 0
+        ;;
+        *)    # unknown option
+            echo "Unkown option $1"
+            show_help
+            exit 1
+        ;;
+    esac
+    done
+}
+
+build_locally() {
+    echo "Building Docker image locally"
+
+    cd ../
+    # using the -f flag allows us to include files from a directory out of the 'context'
+    # we need it because the Dockerfile is in build dir and sources are in driver dir
+
+    # NOTE: building while the snx VPN is running could cause network issues that will fail building the image
+    docker build -f build_tools/nvmesh-csi-plugin.dockerfile . --tag excelero/nvmesh-csi-plugin:v0.0.1
+
+    if [ $? -ne 0 ]; then
+        echo "Docker image build failed"
+        exit 1
+    fi
+}
+
+build_on_remote_machines() {
+    echo "Building on remote machines ${servers[@]}"
+
+    for server in ${servers[@]}; do
+        echo "Sending sources to remote machine ($server).."
+        rsync -r ../ $server:~/nvmesh_csi_driver/
+    done
+
+    for server in ${servers[@]}; do
+        echo "running local build.sh on remote machine ($server).."
+        ssh $server "cd ~/nvmesh_csi_driver/build_tools/ ; ./build.sh"
+    done
+}
+
+### MAIN ###
+parse_args $@
+
+if [ ${#servers[@]} -eq 0 ];then
+    build_locally
+    exit 0
+else
+    build_on_remote_machines
 fi
