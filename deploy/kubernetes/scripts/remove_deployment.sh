7b932855d36187ca487af93597c96337c3762be4
Gil Vitzinger <gil.vitzinger@excelero.com>
2019-09-19 16:57:06 +0300

    remove_deployment script updated
    * added command  to remove  service nvmesh-csi-controller
    
    Change-Id: I3c38dc0ad296756e49dd994f354b78259977ae25
    Signed-off-by: Gil <gil.vitzinger@excelero.com>

--- deploy/kubernetes/scripts/remove_deployment.sh
+++ deploy/kubernetes/scripts/remove_deployment.sh
@@ -3,4 +3,5 @@ kubectl delete service nvmesh-csi-controller -n nvmesh-csi
 kubectl delete statefulsets nvmesh-csi-controller -n nvmesh-csi
 kubectl delete daemonset nvmesh-csi-node-driver -n nvmesh-csi
 kubectl delete storageclass nvmesh-concatenated nvmesh-raid0 nvmesh-raid1 nvmesh-raid10 nvmesh-ec -n nvmesh-csi
+kubectl delete service nvmesh-csi-controller
 kubectl delete namespace nvmesh-csi
