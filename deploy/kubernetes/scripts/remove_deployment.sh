4a35f7d965844a83f33e076ac852ba2cc19d3af5
Gil Vitzinger <gil.vitzinger@excelero.com>
2019-09-23 09:28:40 +0300

    remove_deployment script - removing RBAC objects
    *added code to remove all RBAC objects in remove_deployment.sh script
    
    Change-Id: I0887755b419c66538f036bca07f8a8a5396bdd84
    Signed-off-by: Gil <gil.vitzinger@excelero.com>

--- deploy/kubernetes/scripts/remove_deployment.sh
+++ deploy/kubernetes/scripts/remove_deployment.sh
@@ -1,8 +1,19 @@
-
-kubectl delete service nvmesh-csi-controller -n nvmesh-csi
+# Delete Driver
 kubectl delete statefulsets nvmesh-csi-controller -n nvmesh-csi
 kubectl delete daemonset nvmesh-csi-node-driver -n nvmesh-csi
+
+# Delete Default Storage Classes
 kubectl delete storageclass nvmesh-concatenated nvmesh-raid0 nvmesh-raid1 nvmesh-raid10 nvmesh-ec -n nvmesh-csi
+
+# Delete RBAC Permission objects
+kubectl delete rolebinding csi-attacher-role-cfg csi-provisioner-role-cfg csi-resizer-role-cfg -n nvmesh-csi
+kubectl delete role external-attacher-cfg external-provisioner-cfg external-resizer-cfg -n nvmesh-csi
+kubectl delete clusterrole external-attacher-runner external-provisioner-runner external-resizer-runner
+kubectl delete clusterrolebinding csi-attacher-role csi-provisioner-role csi-resizer-role
+
+# Delete Service and Drvier objects
 kubectl delete service nvmesh-csi-controller
 kubectl delete CSIDriver nvmesh-csi.excelero.com
+
+# Delete nvmesh-csi Namespace
 kubectl delete namespace nvmesh-csi
