bfefa9ac52bf40180a9ed7940d90fe6073d93c7c
Gil Vitzinger <gil.vitzinger@excelero.com>
2020-03-25 17:53:27 +0100

    K8s Deployment - Refactor
    *example yaml files moved to docs/examples
    *building deployment yaml files now uses the helm template command
    *resources folder removed, the creation of yaml will use the helm/templates/*.yaml files
    
    Change-Id: Ic57ea435464b3db51974e0e5d9f664cb58a7d12f
    Signed-off-by: Gil <gil.vitzinger@excelero.com>

--- deploy/kubernetes/scripts/build_deployment_file.sh
+++ deploy/kubernetes/scripts/build_deployment_file.sh
@@ -1,47 +1,12 @@
-VERSION_FILE_PATH=../../../version
-VERSION=$(cat $VERSION_FILE_PATH)
-
-if [ -z "$VERSION" ]; then
-    echo "Could not find version in $VERSION_FILE_PATH"
-    exit 1
-fi
-
 build_deployment_file() {
-    YAML_SEPARATOR="---"
-    DEPLOYMENT_FILE_PATH="../deployment$deploy_version.yaml"
-
-    echo "Building $DEPLOYMENT_FILE_PATH"
-    ORDERED_LIST_OF_YAML_FILES=(
-        nvmesh-csi-namespace.yaml
-        nvmesh-csi-driver.yaml
-        nvmesh-configmaps.yaml
-        nvmesh-credentials.yaml
-        rbac-permissions.yaml
-        nvmesh-storage-classes.yaml
-        nvmesh-csi-deployment$deploy_version.yaml
-    )
-
-    cd ../resources
-
-    printf "#\n# This file was generated using the 'build_deployment_file.sh' script\n#\n\n" > $DEPLOYMENT_FILE_PATH
-    for filename in ${ORDERED_LIST_OF_YAML_FILES[@]}
-    do
-        echo "Adding $filename"
-        cat $filename >> $DEPLOYMENT_FILE_PATH
-        printf "\n$YAML_SEPARATOR\n" >> $DEPLOYMENT_FILE_PATH
-    done
-
-    echo "Inserting version string \"$VERSION\""
-
-    sed -i "s/<version>/$VERSION/g" $DEPLOYMENT_FILE_PATH
-
+    DEPLOYMENT_FILE_PATH="../deployment-k8s-$k8s_version.yaml"
+    helm template ../helm/nvmesh-csi-driver --set overrideKubeVersion=$k8s_version > $DEPLOYMENT_FILE_PATH
     echo "Deployment file $DEPLOYMENT_FILE_PATH ready"
 }
 
+k8s_versions=("1.15" "1.17")
 
-deployment_versions=("-k8s-1.15" "-k8s-1.17")
-
-for deploy_version in ${deployment_versions[@]}
+for k8s_version in ${k8s_versions[@]}
 do
     build_deployment_file
 done
