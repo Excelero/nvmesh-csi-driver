8624b16cd92e128a4dde4d68c4dd99e01e05b2f9
Gil Vitzinger <gil.vitzinger@excelero.com>
2021-03-04 08:52:24 +0100

    Removed some TODO's

--- test/integration/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# TODO: add test for nvmesh-csi-controller with replicas > 1
-# TODO: add test for IO Comparison from the container and from outside the container
-# TODO: add test for IO performance regression
