6bfe27f57589204b111e5455ee97ac4af949fb66
Gil Vitzinger <gil.vitzinger@excelero.com>
2021-10-19 17:02:44 +0200

    Fix - build.sh image version when commit has a tag
    *Fixed wrong docker image tag when building from a commit that has a version tag

--- build_tools/build.sh
+++ build_tools/build.sh
@@ -8,11 +8,19 @@ ONLY_MANIFESTS=false
 BUILD_CLUSTER_SIM=false
 
 get_version() {
-    VERSION=$(git describe | cut -f1 -d '-')
-    BUILD=$(git describe | cut -f2 -d '-')
-    COMMIT=$(git describe | cut -f3 -d '-')
+    DESCRIBE=$(git describe --long)
+    VERSION=$(echo "$DESCRIBE" | cut -f1 -d '-')
+    BUILD=$(echo "$DESCRIBE" | cut -f2 -d '-')
+    COMMIT=$(echo "$DESCRIBE" | cut -f3 -d '-')
 
-    DRIVER_VERSION="${VERSION}-${BUILD}"
+    if [ "$DESCRIBE" != "$(git describe)" ];then
+        # git describe == git describe --long - this means we are building a commit that has a version tag
+        DRIVER_VERSION="${VERSION}"
+    else
+        DRIVER_VERSION="${VERSION}-${BUILD}"
+    fi
+
+    echo "DRIVER_VERSION=$DRIVER_VERSION  DESCRIBE=$DESCRIBE"
 }
 
 get_version
