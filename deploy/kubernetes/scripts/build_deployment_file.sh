1c0770b1fc7fd827091df1e9c7b668e85898bb94
Gil Vitzinger <gvitzinger@nvidia.com>
2023-03-30 13:34:48 +0200

    Dev Tools: Generate values-dev if not exists
    Change-Id: I91b357220198c32088d86746390de3423b129b3a
    Signed-off-by: Gil <gvitzinger@nvidia.com>

--- deploy/kubernetes/scripts/build_deployment_file.sh
+++ deploy/kubernetes/scripts/build_deployment_file.sh
@@ -1,13 +1,24 @@
+
+RELEASE_NAME=nvmesh-csi-driver
+VALUES_PATH="../helm/nvmesh-csi-driver/values.yaml"
+VALUES_DEV_PATH="../helm/nvmesh-csi-driver/values-dev.yaml"
+
 build_deployment_file() {
     DEPLOYMENT_FILE_PATH="../deployment.yaml"
-    RELEASE_NAME=nvmesh-csi-driver
     helm template $RELEASE_NAME ../helm/nvmesh-csi-driver --namespace nvmesh-csi --set overrideKubeVersion=$k8s_version > $DEPLOYMENT_FILE_PATH
     echo "Deployment file $DEPLOYMENT_FILE_PATH ready"
 }
 
 build_dev_deployment_file() {
     DEPLOYMENT_FILE_PATH="../deployment_dev.yaml"
-    RELEASE_NAME=nvmesh-csi-driver
+
+    if [ ! -f $VALUES_DEV_PATH ]; then
+        cp $VALUES_PATH $VALUES_DEV_PATH
+
+        sed -i '1 i\# DEV OVERRIDES\nappVersion: v1.3.0-dev\nk8sVersion: "1.25"\n\n' $VALUES_DEV_PATH
+        echo "Generated new $VALUES_DEV_PATH file"
+    fi
+
     helm template $RELEASE_NAME ../helm/nvmesh-csi-driver -f ../helm/nvmesh-csi-driver/values-dev.yaml --namespace nvmesh-csi --set overrideKubeVersion=$k8s_version > $DEPLOYMENT_FILE_PATH
     echo "Dev Deployment file $DEPLOYMENT_FILE_PATH ready"
 }
