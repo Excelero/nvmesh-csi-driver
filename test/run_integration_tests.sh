9490414f1e6605a31c3f6657a071b3e7d859a02a
Gil Vitzinger <gvitzinger@nvidia.com>
2023-03-30 13:05:26 +0200

    Tests - Integration tests now run localy
    *reverted integration tests to run localy instead of as a k8s job
    *this prevents some issues with runin exec on containers from within the job container
    *added usage examples to the run_integration_tests.sh script
    
    Change-Id: I853d0b0162b52207784dab7b4fe9d2545ee6aeee
    Signed-off-by: Gil <gvitzinger@nvidia.com>

--- test/run_integration_tests.sh
+++ test/run_integration_tests.sh
@@ -3,6 +3,29 @@
 cd ../
 
 export TEST_CONFIG_PATH="test/config.yaml"
-python2 -m unittest discover test/integration
+
+print_usage() {
+    echo -e "usage: $0 [test-name] [--clear-env]"
+
+    echo -e "\n\nOptions:"
+    echo -e "\t --clear-env     Clear Kubernetes and NVMesh from previous test resources - this wll delete all NVMesh volumes"
+    echo -e "\t test-name       name of a specific test to run. can be module name, TesCase class, or a test function. see examples below"
+
+    echo -e "\nExamples:"
+    echo -e "\t $0 --clear-env"
+    echo -e "\t $0 test.integration.tests.test_storage_class_params"
+    echo -e "\t $0 test.integration.tests.test_storage_class_params.TestStorageClassParameters"
+    echo -e "\t $0 test.integration.tests.test_storage_class_params.TestStorageClassParameters.test_raid0_no_params"
+}
+
+if [ $# -eq 0 ]; then
+    python2 -m unittest discover test/integration 
+elif [ $1 == "--help" ] || [ $1 == "-h" ]; then
+    print_usage
+elif [ $1 == "--clear-env" ]; then
+    python2 -m unittest test.integration.tests.clear_test_environment
+else
+    python2 -m unittest $@
+fi
 
 exit $?
